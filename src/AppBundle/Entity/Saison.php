cdfea1d555eae11a165ee6b72db49adc2d0a7e3e
jennulrich <31631277+jennulrich@users.noreply.github.com>
2018-02-25 15:03:58 +0100

    ajout entité saison + fixture (jenn) (#2)

---
+++ src/AppBundle/Entity/Saison.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Saison
+ *
+ * @ORM\Table(name="saison")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SaisonRepository")
+ */
+class Saison
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="nb_saison", type="float")
+     */
+    private $nbSaison;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nbSaison
+     *
+     * @param float $nbSaison
+     *
+     * @return Saison
+     */
+    public function setNbSaison($nbSaison)
+    {
+        $this->nbSaison = $nbSaison;
+
+        return $this;
+    }
+
+    /**
+     * Get nbSaison
+     *
+     * @return float
+     */
+    public function getNbSaison()
+    {
+        return $this->nbSaison;
+    }
+}
+
