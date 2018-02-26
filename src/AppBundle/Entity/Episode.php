478008fa6bb0bbea1057bfa4848ed20a9c2bc07b
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-02-26 11:19:37 +0100

    ajout entité episode (jenn)

---
+++ src/AppBundle/Entity/Episode.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Episode
+ *
+ * @ORM\Table(name="episode")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EpisodeRepository")
+ */
+class Episode
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="nb_episode", type="float")
+     */
+    private $nbEpisode;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nbEpisode
+     *
+     * @param float $nbEpisode
+     *
+     * @return Episode
+     */
+    public function setNbEpisode($nbEpisode)
+    {
+        $this->nbEpisode = $nbEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get nbEpisode
+     *
+     * @return float
+     */
+    public function getNbEpisode()
+    {
+        return $this->nbEpisode;
+    }
+}
+
