1c81f58bdba5a7f44d2cf15804779c0ec0635ea8
Amandine <amandine.lesaint@yahoo.com>
2018-02-26 11:45:17 +0100

    envoi modif film

--- src/AppBundle/Controller/FilmController.php
+++ src/AppBundle/Controller/FilmController.php
@@ -3,8 +3,14 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Film;
+use AppBundle\Form\FilmType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class FilmController extends Controller
 {
@@ -18,6 +24,55 @@ class FilmController extends Controller
             ->findAll();
         return $this->render('film/listFilm.html.twig', [
             'films' => $films
+        ]);
+    }
+
+    /**
+     * @Route("/films/add", name="film_add")
+     */
+    public function addAction(Request $request)
+    {
+        $films = new Film();
+
+        $form = $this->createForm(FilmType::class, $films);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute('film_list');
+        }
+
+        return$this->render('film/addFilm.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    //Modifier Film
+    /**
+     * @Route("/film/{id}/edit", name="film_edit", requirements={"id"="\d+"})
+     */
+    public function EditAction($id, Request $request)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $addFilm = $em->getRepository(Film:: class)
+            ->find($id);
+        $form = $this->createForm(FilmType::class, $addFilm);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+            return $this->redirectToRoute('film_list');
+        }
+
+        return $this->render('film/editFilm.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
