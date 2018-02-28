108dd1c0452514f3e81f1f44fb260c572b9a64e9
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-02-28 09:24:30 +0100

    ajout de la fonction upload pour les images

---
+++ src/AppBundle/EventListener/UploadFileListener.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jennou
+ * Date: 27/02/2018
+ * Time: 16:24
+ */
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use AppBundle\Entity\Film;
+use AppBundle\Service\FileUpload;
+
+class UploadFileListener
+{
+    private $uploader;
+
+    public function __construct(FileUpload $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    private function uploadFile($entity)
+    {
+        // upload only works for Product entities
+        if (!$entity instanceof Film) {
+            return;
+        }
+
+        $file = $entity->getImage();
+
+        // only upload new files
+        if (!$file instanceof UploadedFile) {
+            return;
+        }
+
+        $fileName = $this->uploader->upload($file);
+        $entity->setImage($fileName);
+    }
+}
