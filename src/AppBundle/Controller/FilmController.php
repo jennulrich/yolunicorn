05bb4d8419c6d6980398561a330998f824d39c24
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-03-02 21:56:02 +0100

    css partie admin

--- src/AppBundle/Controller/FilmController.php
+++ src/AppBundle/Controller/FilmController.php
@@ -3,18 +3,17 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Film;
-use AppBundle\Form\FilmType;
 use AppBundle\Manager\FilmManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 
 class FilmController extends Controller
 {
+    // Affiche le détail d'un film à l'user
     /**
      * @Route("/films/{id}", name="film_view", requirements={"id"="\d+"})
      */
@@ -30,6 +29,7 @@ class FilmController extends Controller
         ]);
     }
 
+    // Affiche la liste des films à l'user
     /**
      * @Route("/films", name="film_list")
      */
@@ -43,92 +43,7 @@ class FilmController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/films/add", name="film_add")
-     */
-    public function addAction(Request $request)
-    {
-        $films = new Film();
-
-        $form = $this->createForm(FilmType::class, $films);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $task = $form->getData();
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($task);
-            $em->flush();
-
-            return $this->redirectToRoute('film_list');
-        }
-
-        return$this->render('film/addFilm.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    //Modifier Film
-    /**
-     * @Route("/films/{id}/edit", name="film_edit", requirements={"id"="\d+"})
-     */
-    public function EditAction($id, Request $request)
-    {
-        $em=$this->getDoctrine()->getManager();
-        $addFilm = $em->getRepository(Film:: class)
-            ->find($id);
-        $addFilm->setImage(null);
-        $addFilm->setVideo(null);
-        $form = $this->createForm(FilmType::class, $addFilm);
-
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $task = $form->getData();
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($task);
-            $em->flush();
-            return $this->redirectToRoute('film_list');
-        }
-
-        return $this->render('film/editFilm.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/films/{id}/delete", name="film_delete", requirements={"id"="\d+"})
-     */
-    public function DeleteAction(Film $film) {
-        $em=$this->getDoctrine()->getManager();
-        $em->remove($film);
-        $em->flush();
-
-        return $this->redirectToRoute('film_list');
-    }
-
-    /**
-     * @Route("/films/{id}/image", name="film_image", requirements={"id"="\d+"})
-     */
-    public function ImageViewAction($id) {
-        $em = $this->getDoctrine()->getManager();
-        $film = $em->getRepository(Film::class)
-            ->find($id);
-        $file = $this->getParameter("files_directory")."/".$film->getImage();
-        return new BinaryFileResponse($file);
-    }
-
-    /**
-     * @Route("/films/{id}/video", name="film_video", requirements={"id"="\d+"})
-     */
-    public function VideoViewAction($id) {
-        $em = $this->getDoctrine()->getManager();
-        $film = $em->getRepository(Film::class)
-            ->find($id);
-        $file = $this->getParameter("files_directory")."/".$film->getVideo();
-        return new BinaryFileResponse($file);
-    }
-
-
-    // Recherche
+    // Recherche par titre de film
     /**
      * @Route("/films/search", name="film_search")
      */
